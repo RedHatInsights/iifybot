7b9fdaf5d099ceb3085c13d1db3eda4ac188b17c
Kent Aycoth <kaycoth@redhat.com>
2023-06-28 09:16:16 -0400

    Fixing and updating docker file and main code

--- utilities.py
+++ utilities.py
@@ -144,7 +144,7 @@ def rhsa_data_parser(rhsa_results, list_of_cves):
     results = []
     for cve in list_of_cves:
         for r in rhsa_results:
-            if "name" in r.keys() == cve:
+            if "name" in r.keys() and str(r["name"])== cve:
                 if "affected_release" in r.keys():
                     for data in r["affected_release"]:
                         if data["product_name"] == "Red Hat Enterprise Linux 8":
